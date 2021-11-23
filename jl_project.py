43634a3775ac68729f6904b7f0623b665b9e3c50
Henry Smith <henry.smith@yale.edu>
2021-11-23 07:58:06 -0500

    Small code changes

--- jl_project.py
+++ jl_project.py
@@ -1,9 +1,16 @@
 import numpy as np
 
 def compute_jl_projection(V, epsilon):
-  """Projects the set of points V in \mathbb{R}^d onto a lower subspace such that the distance between any two points in V 
+  """Projects the set of points V in \mathbb{R}^d onto a lower subspace such that the distance between any two vectors in V 
   is preserved by a factor of \epsilon; note that the dimension of the subspace onto which we project is a function of both
-  the number of points in V as well as \epsilon"""
+  the number of vectors in V as well as \epsilon
+  
+  - V: A numpy ndarray whose rows are the set of vectors in \mathbb{R}^d (V has dimension n \times d)
+  - epsilon: A value in (0, 1) which controls the tolerance for distance distortion when projecting the vectors from \mathbb{R}^d to a k-dimensional subspace of \mathbb{R}^d;
+  for more, see https://en.wikipedia.org/wiki/Johnson%E2%80%93Lindenstrauss_lemma
+  
+  return: An n \times k numpy ndarray whose rows are the the vectors in the k-dimensional subspace which satisfy the distance distortion property
+  """
 
   if not isinstance(V, np.ndarray):
     raise TypeError("V must be a numpy nd array.")
@@ -25,7 +32,8 @@ def compute_jl_projection(V, epsilon):
   # Compute the original squared \ell^2 distances between points in V
   orig_distances = compute_distances(V)
 
-  mismatches = n
+  mismatches = n**2
+  # Array for projections onto k-dimensional subspace
   proj = np.zeros((k, n))
   iterator = 0
 
@@ -44,12 +52,12 @@ def compute_jl_projection(V, epsilon):
     mismatches = n**2 - np.sum(np.logical_and(proj_distances >= (1 - epsilon)*orig_distances,  proj_distances <= (1 + epsilon)*orig_distances))
 
   print(f"Projection found after {iterator} iterations.")
-  return k, proj
+  return k, proj.T
 
 def compute_distances(M):
   """Computes the squared Euclidean distance between each pair of points in M,
    where the rows of M are assumed to be the points; returns an upper triangular
-   matrix whose ijth entry is the [squared Euclidean] distance between points i and j."""
+   matrix whose ijth entry, i < j is the [squared Euclidean] distance between points i and j."""
 
   # Get the number of points in our dataset
   n_points = int(M.shape[0])
